dcc27bea4b445d56981c078a6118651f3eed1234
Rhuturaj-Rogaye <rhuturajrogaye@gmail.com>
2025-07-22 18:53:18 +0530

    add code47 and code48 file

--- code47.cpp
+++ code47.cpp
@@ -1,14 +1,16 @@
-#include<iostream>
 #include<vector>
+#include<iostream>
+
 
 using namespace std;
 
 int MajorityEle(vector<int> &nums){
     int n = nums.size();
     int freq=0 , ans=0;
+    
     for (int i = 0; i < n; i++)
     {
-        if (freq==0)
+        if (freq== 0)
         {
             ans= nums[i];
         }
@@ -18,13 +20,25 @@ int MajorityEle(vector<int> &nums){
         }else{
             freq--;
         }
-        
     }
-    return ans;
-}
+        int count=0;
+        for(int val: nums){
+            if (val==ans)
+            {
+                count++;
+            }
+        }
 
+        if (count>n/2)
+        {
+            return ans;
+        }else{
+            return -1;
+        }
+          
+}
 int main() {
-    vector<int> nums = {1, 2, 2, 2, 2, 2, 1, 1, 1};
+    vector<int> nums = {1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 2, 1};
     cout<<MajorityEle(nums);
     return 0;
 }
